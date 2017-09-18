70921aab52fe55ec5b606b870fa6f03b2f744b24
Jeannie <jflaherty@utexas.edu>
2017-09-18 15:10:09 -0500

    update port call

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
@@ -37,6 +38,6 @@ require("./app/routing/htmlRouting")(app);
 
 // Starts the server to begin listening
 // =============================================================
-app.listen(PORT, function() {
+app.listen(port, function() {
   console.log("App listening on PORT " + PORT);
 });
