35fddfd15c12629bf67e0622cbc61ef0bf87ff84
Jeannie <jflaherty@utexas.edu>
2017-10-01 08:11:53 -0500

    survey

--- app/routing/htmlRouting.js
+++ app/routing/htmlRouting.js
@@ -1,20 +1,19 @@
-
-   var path = require("path");
-
-
-   // ===============================================================================
-   // ROUTING
-   // ===============================================================================
- module.exports = function(app) {
-
-     app.get("/survey", function(req, res) {
-       res.sendFile(path.join(__dirname, "/../public/survey.html"));
-     });
-
-     app.get("*", function(req, res){
-       res.sendFile(path.join(__dirname, "/../public/home.html"));
-     });
+var path = require("path");
 
 
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
 
-   };
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+  });
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
+  });
+
+
+
+};
