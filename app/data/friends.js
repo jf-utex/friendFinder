fdea074a0fed751fbbb73431a1f697e0f648b41a
Jeannie <jflaherty@utexas.edu>
2017-09-30 21:25:06 -0500

    updated routes and fixed links

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,9 +1,7 @@
-
- var friends = [
-  {
+var friends = [{
     "name": "Alfalfa",
-    "photo":"https://en.wikipedia.org/wiki/File:Alfalfa_gip.jpg",
-    "scores":[
+    "photo": "https://en.wikipedia.org/wiki/File:Alfalfa_gip.jpg",
+    "scores": [
       "3",
       "4",
       "5",
@@ -11,57 +9,57 @@
       "1",
       "6",
       "7",
-    "7",
+      "7",
       "8",
       "2",
     ]
   },
   {
     "name": "Big Bird",
-  "photo":"https://upload.wikimedia.org/wikipedia/en/b/b3/Flickr_Big_Bird.jpg",
-  "scores":[
-    "3",
-    "4",
-    '5',
-    '2',
-    '1',
-    '6',
-    '7',
-    '7',
-    '8',
-    '2',
-  ]
-},{
-  "name": "Shirley Temple",
-  "photo":"https://upload.wikimedia.org/wikipedia/commons/a/a2/ShirleyTemple_in_1944.jpg",
-  "scores":[
-    '3',
-    '4',
-    '5',
-    '2',
-    '1',
-    '6',
-    '7',
-    '7',
-    '8',
-    '2',
-  ]
-},{
-  "name": "Lucy",
-  "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Lucille_Ball_1944crop.jpg/440px-Lucille_Ball_1944crop.jpg",
-  "scores":[
-    '3',
-    '4',
-    '5',
-    '2',
-    '1',
-    '6',
-    '7',
-    '7',
-    '8',
-    '2,'
-  ]
-}
+    "photo": "https://upload.wikimedia.org/wikipedia/en/b/b3/Flickr_Big_Bird.jpg",
+    "scores": [
+      "3",
+      "4",
+      '5',
+      '2',
+      '1',
+      '6',
+      '7',
+      '7',
+      '8',
+      '2',
+    ]
+  }, {
+    "name": "Shirley Temple",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/a/a2/ShirleyTemple_in_1944.jpg",
+    "scores": [
+      '3',
+      '4',
+      '5',
+      '2',
+      '1',
+      '6',
+      '7',
+      '7',
+      '8',
+      '2',
+    ]
+  }, {
+    "name": "Lucy",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Lucille_Ball_1944crop.jpg/440px-Lucille_Ball_1944crop.jpg",
+    "scores": [
+      '3',
+      '4',
+      '5',
+      '2',
+      '1',
+      '6',
+      '7',
+      '7',
+      '8',
+      '2,'
+    ]
+  }
 
 ];
 module.exports = friends;
--- app/routing/apiRouting.js
+++ app/routing/apiRouting.js
@@ -1,60 +1,59 @@
+var friends = require("../data/friends");
 
-   var friends = require("../data/friends");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
 
-   // ===============================================================================
-   // ROUTING
-   // ===============================================================================
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
 
-   module.exports = function(app) {
-     // API GET Requests
-     // Below code handles when users "visit" a page.
-     // In each of the below cases when a user visits a link
-     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-     // ---------------------------------------------------------------------------
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
 
-     app.get("/api/friends", function(req, res) {
-       res.json(friends);
-     });
+  app.post("/api/friends", function(req, res) {
 
-     app.post("/api/friends", function(req, res) {
+    var bestMatch = {
+      name: "",
+      photo: "",
+      bestDiff: 1000
 
-       var bestMatch = {
-         name: "",
-         photo: "",
-         bestDiff: 1000
+    };
+    var userData = req.body;
+    var userScores = userData.scores;
+    var totalDifference = 0;
 
-       };
-       var userData = req.body;
-       var userScores = userData.scores;
-       var totalDifference = 0;
-
-       for (var i = 0; i < friends.length; i++) {
-         console.log(friends[i].name);
-        //  var currentFriend = friends[i];
-         totalDifference = 0;
-
+    for (var i = 0; i < friends.length; i++) {
+      console.log(friends[i].name);
+       var currentFriend = friends[i];
+      totalDifference = 0;
 
 
 
-         for (var k = 0; k < friends[i].scores[k]; k++) {
+
+      for (var k = 0; k < friends[i].scores[k]; k++) {
 
 
-          //  var currentFriendScore = currentFriend.scores[k];
-          //  var currentUserScore = userScores[k]
-           totalDifference += Math.abs(parseInt(userScores[k]) - parseInt(friends[i].scores[k]));
+         var currentFriendScore = currentFriend.scores[k];
+         var currentUserScore = userScores[k]
+        totalDifference += Math.abs(parseInt(userScores[k]) - parseInt(friends[i].scores[k]));
 
-         }
-         if (totalDifference <= bestMatch.bestDiff) {
-           bestMatch.name = currentFriend.name;
-           bestMatch.photo = currentFriend.photo;
-           bestMatch.bestDiff = totalDifference;
-         }
+      }
+      if (totalDifference <= bestMatch.bestDiff) {
+        bestMatch.name = currentFriend.name;
+        bestMatch.photo = currentFriend.photo;
+        bestMatch.bestDiff = totalDifference;
+      }
 
-       }
-       friends.push(userData);
+    }
+    friends.push(userData);
 
 
-       // respond back with the best match
-       res.json(bestMatch);
-     });
-   };
+    // respond back with the best match
+    res.json(bestMatch);
+  });
+};
